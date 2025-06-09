80efc8ba6ee984afb3a374394ecebdf0581825f2
Dillan Andrey <dillan.fontecha432@gmail.com>
2025-06-09 14:01:57 -0500

    New_Data_Base_Neon_v3

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -31,17 +31,12 @@ def create_app(config_name='default'):
     app.config['JWT_SECRET_KEY'] = 'supersecretkey'
     app.config['PROPAGATE_EXCEPTIONS'] = True
 
-    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
-    'pool_pre_ping': True
-}
-
-    db = SQLAlchemy(app)
-
     # Inicializar extensiones
     db.init_app(app)
     Migrate(app, db)
     JWTManager(app)
     CORS(app)
+    SQLAlchemy(app)
 
     # Configuración de la API RESTful
     api = Api(app,
